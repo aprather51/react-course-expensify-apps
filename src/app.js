2dd7a86bab22992e5ce261f30159b60ad5ac5bf2
aprather51 <apratherjr@gmail.com>
2017-11-18 08:22:08 -0600

    Removed dummy data (dispatch & store)

--- src/app.js
+++ src/app.js
@@ -8,26 +8,8 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expense';
 import './style/main.scss'; 
 import 'react-dates/lib/css/_datepicker.css';
+
 const store = configureStore();
-
-//Simulation Area -----------------------------
-
-//Dispatching Simulation Items
-store.dispatch(addExpense({ description: 'Water Bill', amount: 5000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 11000, createAt: 11000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 122540 }));
-
-//Displaying Item in Browser Console --
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(store.getState());
-console.log(visibleExpenses);
-
-// store.dispatch(setTextFilter('water'));
-
-// setTimeout(function() {
-//     store.dispatch(setTextFilter('Bill'));
-// }, 3000);
 
 const jsx = (
     <Provider store={store}>
