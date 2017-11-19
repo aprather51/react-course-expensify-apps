f3cfaab4623078beec61b33d335e6fd91e3d053c
aprather51 <apratherjr@gmail.com>
2017-11-19 08:11:47 -0600

    Set system format for time and amount

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItems = ({id, description, amount, createAt}) => (
     <div>
       <Link to={`/edit/${id}`}>
         <h3>{description} </h3>
       </Link> 
-      <p>{amount} - {createAt}</p>
+      <p>
+        {numeral(amount / 100).format('$0,0.00')} 
+        - 
+        {moment(createAt).format('dddd, MMMM D YYYY')}</p>
       <hr/>
     </div>
     
