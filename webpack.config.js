9747a03b7cfd1fd8e9591f0aaefefd24c7346a70
aprather51 <apratherjr@gmail.com>
2017-11-18 20:50:48 -0600

    Setup Development Environment -> devDependencies and dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
     return{    
         entry: './src/app.js',
         output:{
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module:{
@@ -43,10 +43,11 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map', 
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            port: 3000,
+             port: 3000,
             open: true,
             openPage: '',
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     };
 };
